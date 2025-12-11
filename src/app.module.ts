df052fb5bb401d00359b68fc1cc1cbacc6a69c52
davidMarostica <davidmarosticasilvasilva25@gmail.com>
2025-12-11 12:46:04 -0300

    Criação do pasta  Categoria

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CategoriaModule } from './categiria/categoria.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'generetion',
+      password: '123',
+      database: 'db_blogpessoal',
+      autoLoadEntities: true,
+      synchronize: true,
+      logging: true,
+    }),
+    CategoriaModule,
+  ],
 })
 export class AppModule {}
